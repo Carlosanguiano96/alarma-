fc6e4e8cca1f7cde0fd2cba9ad949543e2489c3b
Carlosanguiano96 <36653880+Carlosanguiano96@users.noreply.github.com>
2018-03-15 20:30:38 -0600

    Add files via upload

---
+++ alarm3.ino
@@ -0,0 +1,41 @@
+
+ 
+int contacto = 2;  //Pin asignado al reed switch
+int led= 13;       //Pin asignado al LED
+ 
+
+void setup() // Se ejecuta cada vez que el Arduino se inicia
+{
+  pinMode(contacto,INPUT); //El reed switch como una entrada
+  pinMode(led, OUTPUT);    //El LED como una salida
+ 
+}
+ 
+
+void loop() 
+{           
+ 
+  // Si el iman se acerca al reed switch
+  if (digitalRead(contacto)==HIGH){
+ 
+    //Ciclo for que va de 0 a 50, el contador esta
+    //  en la variable a, se repite mientras a sea
+    //  menor a 50
+    for(int a=0; a<50; a++){
+ 
+      digitalWrite(led,HIGH); //Prende el LED
+      delay(50); //Tiempo
+      digitalWrite(led,LOW); //Apaga el LED
+      delay(50); //Tiempo
+ 
+    }
+ 
+  // Si el iman esta lejos del reed switch
+  }else{  
+ 
+    digitalWrite(led,LOW); //Mantiene apagado el LED
+ 
+  } 
+}
+ 
+
